47b659f5b20d739cf9d0fd59728b43969471041b
Thiago Silvestre <thiago.silvestre@gruponewway.com.br>
2019-02-20 00:06:06 -0300

    Including example of violation of the LSP principle

---
+++ 3-LSP/LSP-Violacao/Retangulo.php
@@ -0,0 +1,18 @@
+﻿<?php
+class Retangulo
+{
+    public $largura;
+    public $altura;
+ 
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+  
+    public function setLargura($largura) {
+        $this->largura = $largura;
+    }
+  
+    public function area() {
+        return $this->altura * $this->largura;
+    }
+}
---
+++ 3-LSP/README.md
@@ -0,0 +1,12 @@
+# SOLID
+
+
+| L |
+| :---: |
+| LSP |
+| Liskov Substitution Principle |
+
+
+Classes derivadas devem poder ser substituídas por suas classes base.
+
+Criado por Barbara Liskov, em 1988.
